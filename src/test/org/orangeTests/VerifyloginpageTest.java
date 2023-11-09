bd07b0caea99ec852a19177b81c282d1d89d44ec
Neha <neha@Nehas-MacBook-Air.local>
2023-11-09 10:31:11 +0530

    Added ExtentReport support

--- src/test/org/orangeTests/VerifyloginpageTest.java
+++ src/test/org/orangeTests/VerifyloginpageTest.java
@@ -1,73 +1,94 @@
 package test.org.orangeTests;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
 import pageFactoryClasses.LoginPageFactory;
 import java.time.Duration;
 
-  
-    
 public class VerifyloginpageTest {
 	private WebDriver driver;
 	private String baseUrl;
 	LoginPageFactory loginPage;
-    private static final Logger log = LogManager.getLogger(VerifyloginpageTest.class.getName());
+	private ExtentReports extent;
+	private static final Logger log = LogManager.getLogger(VerifyloginpageTest.class.getName());
 
-	@BeforeMethod
+	@BeforeClass
 	public void setup() {
-		driver = new ChromeDriver();
-		baseUrl = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
-	
-	    // Create an object for the Page Class
-	    loginPage = new LoginPageFactory(driver);
-	        
-	    driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(10000));
-        driver.get(baseUrl);
-    }  
-  
-    //To verify login is successful 
-    @Test(priority=1)
-    public void verify1() throws InterruptedException {
-    	loginPage.enterUserNameCode("Admin");
-    	loginPage.enterPasswordInputCode("admin123");
-        loginPage.clickLoginButton();
-        boolean actual_adminText=loginPage.findDashBorad();
-        Assert.assertTrue(actual_adminText);
-        log.info("Got the stuff done");
-        log.debug("Got debug stuff done");
-     }
-    
-    //To verify that error message occur when username is wrong     
-    @Test(priority=2)	    
-    public void verify2() throws InterruptedException {
-    	loginPage.enterUserNameCode("ad");
-    	loginPage.enterPasswordInputCode("admin123");
-        loginPage.clickLoginButton();
-        String actualError=loginPage.findErrorMsg();
-        String expectedError="Invalid credentials";
-        Assert.assertEquals(actualError,expectedError);     
+		extent = new ExtentReports();
+		ExtentSparkReporter spark = new ExtentSparkReporter("target/sparkReport.html");
+		extent.attachReporter(spark);
 	}
 
-   //To verify that error message occur when password is wrong 
-   @Test(priority=3)
-   public void verify3() throws InterruptedException {
-    	loginPage.enterUserNameCode("admin");
-    	loginPage.enterPasswordInputCode("ad123");
-        loginPage.clickLoginButton();
-        String actualError=loginPage.findErrorMsg();
-        String expectedError="Invalid credentials";
-        Assert.assertEquals(actualError,expectedError);    
-    }
-   
-    @AfterMethod
-    public void tearDown() throws InterruptedException {
-        Thread.sleep(3000);
-        driver.quit();
-    }		
+	@BeforeMethod
+	public void setupBeforeEachTest() {
+		driver = new ChromeDriver();
+		baseUrl = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
+
+		// Create an object for the Page Class
+		loginPage = new LoginPageFactory(driver);
+
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofMillis(10000));
+		driver.get(baseUrl);
+	}
+
+	// To verify login is successful
+	@Test(priority = 1)
+	public void validCredentials() throws InterruptedException {
+		loginPage.enterUserNameCode("Admin");
+		loginPage.enterPasswordInputCode("admin123");
+		loginPage.clickLoginButton();
+		boolean actual_adminText = loginPage.findDashBorad();
+		Assert.assertTrue(actual_adminText);
+		extent.createTest("VerifyloginpageTest.validCredentials").log(Status.PASS,
+				"VerifyloginpageTest.validCredentials Passed!");
+
+		log.info("Got the stuff done");
+		log.debug("Got debug stuff done");
+	}
+
+	// To verify that error message occur when username is wrong
+	@Test(priority = 2)
+	public void invalidUsername() throws InterruptedException {
+		loginPage.enterUserNameCode("ad");
+		loginPage.enterPasswordInputCode("admin123");
+		loginPage.clickLoginButton();
+		String actualError = loginPage.findErrorMsg();
+		String expectedError = "Invalid credentials";
+		Assert.assertEquals(actualError, expectedError);
+		extent.createTest("VerifyloginpageTest.invalidUsername").log(Status.PASS,
+				"VerifyloginpageTest.invalidUsername Passed!");
+	}
+
+	// To verify that error message occur when password is wrong
+	@Test(priority = 3)
+	public void invalidPassword() throws InterruptedException {
+		loginPage.enterUserNameCode("admin");
+		loginPage.enterPasswordInputCode("ad123");
+		loginPage.clickLoginButton();
+		String actualError = loginPage.findErrorMsg();
+		String expectedError = "Invalid credentials";
+		Assert.assertEquals(actualError, expectedError);
+		extent.createTest("VerifyloginpageTest.invalidPassword").log(Status.PASS,
+				"VerifyloginpageTest.invalidPassword Passed!");
+	}
+
+	@AfterMethod
+	public void tearDown() throws InterruptedException {
+		extent.flush();
+		Thread.sleep(3000);
+		driver.quit();
+	}
 }
