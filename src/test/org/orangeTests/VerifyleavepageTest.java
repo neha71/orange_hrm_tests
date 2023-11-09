d71a0aa06c93db78cbe687202512b6b082eda0fa
Neha <neha@Nehas-MacBook-Air.local>
2023-11-09 09:46:44 +0530

    Fix pom and move tests into nested folders

--- src/test/org/orangeTests/VerifyleavepageTest.java
+++ src/test/org/orangeTests/VerifyleavepageTest.java
@@ -1,4 +1,4 @@
-package test;
+package test.org.orangeTests;
 
 public class VerifyleavepageTest {
 
--- src/test/org/orangeTests/VerifyloginpageTest.java
+++ src/test/org/orangeTests/VerifyloginpageTest.java
@@ -1,4 +1,4 @@
-package test;
+package test.org.orangeTests;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
