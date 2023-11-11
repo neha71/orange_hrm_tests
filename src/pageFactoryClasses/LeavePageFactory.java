596a2d8febf2971c3879295267a7798e775d1cbf
Neha <neha@Nehas-MacBook-Air.local>
2023-11-11 15:14:01 +0530

    Add tests for leave page

---
+++ src/pageFactoryClasses/LeavePageFactory.java
@@ -0,0 +1,100 @@
+package pageFactoryClasses;
+
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LeavePageFactory {
+	
+	WebDriver driver;
+
+	// Elements present in this Page
+	@FindBy(xpath = "//*[@id=\"app\"]/div[1]/div[1]/aside/nav/div[2]/ul/li[3]/a/span")
+	private WebElement leaveTab;
+
+	@FindBy(xpath = "//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[1]/div/div[2]/div/div/input")
+	private WebElement fromDate;
+	
+	@FindBy(xpath = "//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[2]/div/div[2]/div/div/input")
+	private WebElement toDate;
+
+	@FindBy(xpath = "(//i[@class='oxd-icon bi-caret-down-fill oxd-select-text--arrow'])[1]")
+	private WebElement selectLeaveStatus;
+	
+	@FindBy(xpath = "(//div[@role='listbox']//child::div)[4]")
+	private WebElement leaveStatusListElem2;
+	
+	/*
+	@FindBy(xpath = "")
+	private WebElement selectLeaveType;
+	
+	@FindBy(xpath = "")
+	private WebElement leaveTypeListElem2;
+	
+	@FindBy(xpath = "//input[@placeholder='Type for hints...']")
+	private WebElement empName;
+
+	@FindBy(xpath = "//*[@role='listbox']")
+	private List<WebElement> empNameAutoSuggestions;
+	
+	@FindBy(xpath = "")
+	private WebElement selectSubUnit;
+	
+	@FindBy(xpath = "")
+	private WebElement subUnitListElem2;
+	*/
+	
+	@FindBy(xpath = "//input[@placeholder='Type for hints...']")
+	private WebElement empName;
+
+	@FindBy(xpath = "//*[@role='listbox']")
+	private List<WebElement> empNameAutoSuggestions;
+	
+	@FindBy(xpath = "//*[@type='submit' and contains(., \"Search\")]")
+	private WebElement searchButton;
+	
+	@FindBy(xpath = "//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div[2]/div[1]/span")
+	private WebElement searchResultHeader;
+	
+	Actions actions;
+
+	// Constructor to initialize this Class and the Elements
+	public LeavePageFactory(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+		actions = new Actions(driver);
+	}	
+
+	public void clickLeaveTab() {
+		leaveTab.click();
+	}
+	
+	public void selectLeaveStatus() {
+		actions.moveToElement(selectLeaveStatus).click().build().perform();
+		leaveStatusListElem2.click();
+	}
+	
+	public void setDates() {
+		fromDate.sendKeys("2022-01-01");
+		toDate.sendKeys("2022-12-31");
+	}
+	
+	public void enterEmpName(String empName) throws InterruptedException {
+		this.empName.sendKeys(empName);
+		Thread.sleep(2000);
+		empNameAutoSuggestions.get(0).click();
+	}
+	
+	public void clickSearchButton() {
+		searchButton.click();
+	}
+	
+	public boolean isSearchSuccessful() {
+		return searchResultHeader.getText().toLowerCase().contains("records found");
+	}
+	
+}
--- src/test/org/orangeTests/VerifyleavepageTest.java
+++ src/test/org/orangeTests/VerifyleavepageTest.java
@@ -1,5 +1,103 @@
 package test.org.orangeTests;
 
-public class VerifyleavepageTest {
+import java.time.Duration;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+import pageFactoryClasses.LeavePageFactory;
+import pageFactoryClasses.LoginPageFactory;
+
+public class VerifyleavepageTest {
+	private WebDriver driver;
+	private String baseUrl;
+	LeavePageFactory leavePage;
+	LoginPageFactory loginPage;
+	private ExtentReports extent;
+	private static final Logger log = LogManager.getLogger(VerifyloginpageTest.class.getName());
+
+	@BeforeClass
+	public void setup() {
+		extent = new ExtentReports();
+		ExtentSparkReporter spark = new ExtentSparkReporter("target/sparkReport.html");
+		extent.attachReporter(spark);
+	}
+
+	@BeforeMethod
+	public void setupBeforeEachTest() {
+		driver = new ChromeDriver();
+		baseUrl = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
+
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofMillis(10000));
+		driver.get(baseUrl);
+
+		// Login to reach the admin page
+		loginPage = new LoginPageFactory(driver);
+		loginPage.enterUserNameCode("Admin");
+		loginPage.enterPasswordInputCode("admin123");
+		loginPage.clickLoginButton();
+
+		// Create an object for Admin Page and click admin tab
+		leavePage = new LeavePageFactory(driver);
+		leavePage.clickLeaveTab();
+	}
+	
+	@Test(priority = 1)
+	public void searchLeaveWithDefaultDates() throws InterruptedException {
+		leavePage.selectLeaveStatus();
+		Thread.sleep(1000);
+		leavePage.clickSearchButton();
+		
+		Thread.sleep(1000);
+		Assert.assertTrue(leavePage.isSearchSuccessful());
+		extent.createTest("VerifyleavepageTest.searchLeaveWithDefaultDates").log(Status.PASS, "VerifyleavepageTest.searchLeaveWithDefaultDates Passed!");
+		log.info("VerifyleavepageTest.searchLeaveWithDefaultDates passed");
+	}
+	
+	@Test(priority = 2)
+	public void searchLeaveWithCustomDates() throws InterruptedException {
+		leavePage.selectLeaveStatus();
+		leavePage.setDates();
+		Thread.sleep(1000);
+		leavePage.clickSearchButton();
+		
+		Thread.sleep(1000);
+		Assert.assertTrue(leavePage.isSearchSuccessful());
+		extent.createTest("VerifyleavepageTest.searchLeaveWithCustomDates").log(Status.PASS, "VerifyleavepageTest.searchLeaveWithCustomDates Passed!");
+		log.info("VerifyleavepageTest.searchLeaveWithCustomDates passed");
+	}
+	
+	@Test(priority = 3)
+	public void searchLeaveWithCustomDatesAndEmployee() throws InterruptedException {
+		leavePage.selectLeaveStatus();
+		leavePage.setDates();
+		leavePage.enterEmpName("Li");
+		Thread.sleep(1000);
+		leavePage.clickSearchButton();
+		
+		Thread.sleep(1000);
+		Assert.assertTrue(leavePage.isSearchSuccessful());
+		extent.createTest("VerifyleavepageTest.searchLeaveWithCustomDatesAndEmployee").log(Status.PASS, "VerifyleavepageTest.searchLeaveWithCustomDatesAndEmployee Passed!");
+		log.info("VerifyleavepageTest.searchLeaveWithCustomDatesAndEmployee passed");
+	}
+	
+	@AfterMethod
+	public void tearDown() throws InterruptedException {
+		extent.flush();
+		Thread.sleep(3000);
+		driver.quit();
+	}
+	
 }
